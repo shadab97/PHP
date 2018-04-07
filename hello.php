012329f42e1d9af2e580f717d1866c384f19e471
Shadab Ali <31549741+shadab97@users.noreply.github.com>
2018-04-07 17:30:38 +0530

    Create hello.php
    very first program in php

---
+++ hello.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>
+    hello World !
+    </title>
+    
+    </head>
+<body>
+    <?php 
+    echo "hello World! learning php "; //echo used to print value to page like cin>> in c++.
+    ?> 
+    
+    
+    </body>
+
+</html>
